94ee28ea33f05d0fa501f7461302fc12c35a7241
Hugo Cabral Tannus <hctannus@gmail.com>
2020-11-24 21:18:03 -0300

    Folha de estilos para página principal.

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
 const app = Vue.createApp({
   data() {
     return {
-      title: 'Desafio Fron-End ConexaLabs'
+      title: 'Localizador de Empresas',
+      companies: [
+        {
+          id: "1234",
+          social: "Conexa Hub de Inovação",
+          cnpj: "342.454.0001-76",
+          address: "Av Brasil 2233, Centro, Goiânia"
+        },
+        {
+          id: "5678",
+          social: "Conexa Hub de Inovação",
+          cnpj: "342.454.0001-76",
+          address: "Av Brasil 2233, Centro, Goiânia"
+        },
+        {
+          id: "9012",
+          social: "Conexa Hub de Inovação",
+          cnpj: "342.454.0001-76",
+          address: "Av Brasil 2233, Centro, Goiânia"
+        },
+        {
+          id: "3456",
+          social: "Conexa Hub de Inovação",
+          cnpj: "342.454.0001-76",
+          address: "Av Brasil 2233, Centro, Goiânia"
+        },
+      ]
     }
   }
 })
